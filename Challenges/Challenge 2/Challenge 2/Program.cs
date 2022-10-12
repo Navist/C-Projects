069437aec9cc86ecfbfe839174a05e50e7a14992
Navist <zachstheman2@hotmail.com>
2022-10-12 12:33:58 -0400

    Challenges

---
+++ Challenges/Challenge 2/Challenge 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Challenge_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string myString;
+
+            // Get string from user
+            Console.WriteLine("Enter a string here: ");
+            myString = Console.ReadLine();
+
+            Console.WriteLine($"You entered {myString}.");
+
+            // Ask them for a character to search for
+            Console.WriteLine("Enter the character to search: ");
+            char searchInput = Console.ReadLine()[0];
+
+            int searchIndex = myString.IndexOf(searchInput);
+
+            // Print the result of their input
+            Console.WriteLine($"Character index of {searchInput} in {myString} is {searchIndex}.");
+
+
+            string firstName;
+            string lastName;
+
+            // Concatenation challenge
+            Console.WriteLine("What is your first name?: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?: ");
+            lastName = Console.ReadLine();
+
+            // Mutliple ways of concatenation
+
+            string fullName = string.Concat(firstName + " " + lastName);
+
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine($"{firstName} {lastName}");
+            Console.WriteLine("{0} {1}", firstName, lastName);
+            Console.WriteLine("{0}", fullName);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Challenges/Challenge 3/Challenge 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Challenge_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = 255;
+            sbyte mySByte = 127;
+            int myInt = 2147483647;
+            uint myUInt = 4294967295;
+            short myInt16 = 32767;
+            ushort myUInt16 = 65535;
+            long myLong = 9223372036854775807;
+            ulong myULong = 18446744073709551615;
+            float myFloat = 3.402823f;
+            double myDouble = 1.79769313486232;
+            char myChar = 'l';
+            bool myBool = false;
+            string myString = "I control text";
+            decimal myDecimal = 1.06565445645654654656446500m;
+
+            // Pretend I printed all of these to show they will print.
+
+
+        }
+    }
+}
