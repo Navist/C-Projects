3a70cdfc9b152437d4d47f3f5dfedd47b10479d3
Navist <zachstheman2@hotmail.com>
2022-10-12 11:59:52 -0400

    More beginner projects

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float myFloat = float.Parse(stringForFloat);
+            int myInt = int.Parse(stringForInt);
+
+            Console.WriteLine($"Float {myFloat}\nInt {myInt}");
+        }
+    }
+}
--- Experimental_Code/Experimental_Code/Program.cs
+++ Experimental_Code/Experimental_Code/Program.cs
@@ -7,34 +7,90 @@ namespace Experimental_Code
 
         static void Main(string[] args)
         {
-            int num1, num2;
-
-            num1 = 13;
-            num2 = 15;
 
 
-            int sum = num1 + num2;
-
-            Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " equals: " + sum);
-
-            double pi = 3.1415;
-            double pl = 5.1;
-
-            double dDiv = pi / pl;
-
-            Console.WriteLine(dDiv);
 
 
-            float f1 = 3.1415f;
-            float f2 = 5.1f;
+            //Parsing strings to int
+            //string myString = "15";
+            //string myString2 = "30";
 
-            float fDiv = f1 / f2;
+            //int num1 = Int32.Parse(myString);
+            //int num2 = Int32.Parse(myString2);
 
-            Console.WriteLine(fDiv);
+            //Console.WriteLine(num1+num2);
 
-            Console.Read();
-            
-            
+
+
+            // Implicit conversion
+            //int num = 123345;
+            //long bigNum = num;
+
+            //float myFloat = 13.37f;
+
+            //string myString = myFloat.ToString();
+
+            //Console.WriteLine(myString);
+
+
+            // Explicit conversion
+            //double myDouble = 13.37;
+            //int myInt;
+
+            //myInt = (int)myDouble;
+
+            //myString = myDouble.ToString();
+
+            //Console.WriteLine(myString);
+
+            //Console.WriteLine(myInt);
+            //Console.Read();
+
+
+
+
+            // Testing the declaration of variables and how to interact with them
+            //int num1, num2;
+
+            //num1 = 13;
+            //num2 = 15;
+
+
+            //int sum = num1 + num2;
+
+            //Console.WriteLine("num1: " + num1 + " + num2: " + num2 + " equals: " + sum);
+
+            //double pi = 3.1415;
+            //double pl = 5.1;
+
+            //double dDiv = pi / pl;
+
+            //Console.WriteLine(dDiv);
+
+
+            //float f1 = 3.1415f;
+            //float f2 = 5.1f;
+
+            //float fDiv = f1 / f2;
+
+            //Console.WriteLine(fDiv);
+
+            //Console.Read();
+
+
+            // Testing the difference between console writes.
+            //Console.Write("Enter a string and press enter: ");
+            //string userInput = Console.ReadLine();
+            //Console.WriteLine("You have entered {0}", userInput);
+
+            //Console.Write("Enter a string and press enter: ");
+            //int asciiValue = Console.Read();
+            //Console.WriteLine("ASCII value is {0}: ", asciiValue);
+            //Console.ReadKey();
+
+
+
+
         }
     }
 }
